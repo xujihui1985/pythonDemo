a7bef7e82f1b185e5c1fae1440738fb77d8b8816
Sean <xujihui1985@gmail.com>
2015-02-14 21:44:30 +0800

    update packageDemo

--- packageDemo/reader.py
+++ packageDemo/reader.py
@@ -1,7 +1,16 @@
+import os
+from packageDemo.compressed import gzip
+
+extension_map = {
+    '.gz': gzip.opener 
+}
+
 class Reader:
     def __init__(self, filename):
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension, open)
         self.filename = filename
-        self.f = open(self.filename, 'rt')
+        self.f = opener(self.filename, 'rt')
 
     def close(self):
         self.f.close()
